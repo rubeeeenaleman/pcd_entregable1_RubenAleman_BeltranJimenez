87ad92addb2029fd6c9ed0c05b2daa416211578b
rubeeeenaleman <rubeeenaleman@gmail.com>
2026-03-16 20:51:59 +0100

    pequeña correccion

--- proyecto_estelar.py
+++ proyecto_estelar.py
@@ -4,28 +4,28 @@ class UnidadesCombateEstelares:
         self.IdCombate = IdCombate
         self.__Clave = Clave
 
-class Nave:
+class Nave(UnidadesCombateEstelares):
 
     def __init__(self, IdCombate: str, Clave: int, Nombre: str, PiezasRepuesto: list):
         super().__init__(IdCombate, Clave)
         self.Nombre = Nombre
         self.PiezasRepuesto = PiezasRepuesto
 
-class EstacionEstelar:
+class EstacionEstelar(UnidadesCombateEstelares, Nave):
     
     def __init__(self, IdCombate: str, Clave: int, Nombre: str, PiezasRepuesto: list,  Tripulacion: int, Pasaje: int): # faltan las enumeraciones.
         super().__init__(IdCombate, Clave, Nombre, PiezasRepuesto)
         self.Tripulacion = Tripulacion
         self.Pasaje = Pasaje
 
-class NaveEstelar:
+class NaveEstelar(UnidadesCombateEstelares, Nave):
 
     def __init__(self, IdCombate: str, Clave: int, Nombre: str, PiezasRepuesto: list, Tripulacion: int, Pasaje: int):
         super().__init__(IdCombate, Clave, Nombre, PiezasRepuesto)
         self.Tripulacion = Tripulacion
         self.Pasaje = Pasaje
 
-class CazaEstelar:
+class CazaEstelar(UnidadesCombateEstelares, Nave):
 
     def __init__(self, IdCombate: str, Clave: int, Nombre: str, PiezasRepuesto: list, Dotacion: int):
         super().__init__(IdCombate, Clave, Nombre, PiezasRepuesto)
